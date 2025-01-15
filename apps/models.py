fc955e00e06f2ec4f87d6373dd15d3780c46cca0
rashidiy <bubbless7456@gmail.com>
2025-01-15 21:08:45 +0500

    fix phone number

--- apps/models.py
+++ apps/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Complaint(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=50)
     text = models.TextField()
