ec9f1a130720abc9a5107e44ef04f088c04c0205
rashidiy <bubbless7456@gmail.com>
2025-01-15 20:37:01 +0500

    fix unique error

--- apps/models.py
+++ apps/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Complaint(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=11)
     text = models.TextField()
