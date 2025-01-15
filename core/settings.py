4536eb5f5e37f5d13b707d5e414d2384434c5802
rashidiy <bubbless7456@gmail.com>
2025-01-15 20:54:22 +0500

    changing database to postgres

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
     }
 }
 
