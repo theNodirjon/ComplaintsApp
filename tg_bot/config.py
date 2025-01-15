e6d4cac773cf88d034ea2e77a1322548d2fea6c4
rashidiy <bubbless7456@gmail.com>
2025-01-15 20:47:46 +0500

    Adding Storage mechanism Redis

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,6 +1,9 @@
 import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio import Redis
+
 from tg_bot.handlers.commands import router as commands_router
 
 
@@ -10,5 +13,6 @@ class Config:
 
 bot = Bot(Config.BOT_TOKEN)
 
-dispatcher = Dispatcher()
+storage = RedisStorage(Redis(), state_ttl=5400, data_ttl=5400)
+dispatcher = Dispatcher(storage=storage)
 dispatcher.include_router(commands_router)
