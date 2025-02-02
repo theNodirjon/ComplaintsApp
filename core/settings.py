a8c25fe151677c5ea4d54746dcf051e90541da3c
Nodir <116571203+theNodirjon@users.noreply.github.com>
2025-02-02 19:54:52 +0500

    settings.py/DATA BASES

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', 5432),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
